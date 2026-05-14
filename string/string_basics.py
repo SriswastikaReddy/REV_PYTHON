83e33266a3a9581823a2a63e46907bd2d7d23a6d
Sriswastika <swastikareddy02@gmail.com>
2026-05-14 00:48:08 -0500

    added string projects and basics

---
+++ string/string_basics.py
@@ -0,0 +1,57 @@
+'''a = 'hello'
+print(a[0])  # h
+print(a[1])  # e
+print(a[2])  # l
+print(a[3])  # l
+print(a[4])  # o'''
+
+'''a = 'hello'
+for i in a:
+    print(i)'''
+
+
+'''a = 'hello'    
+for x in range(len(a)):
+    print(a[x])'''
+    
+    
+  #string slicing  
+'''s1 = "hello world"
+s2 = s1[3:8]
+s3 = s1[-8:-3]
+s4 = s1[7:2:-1]
+s5 = s1[:5]
+print(s2)  # lo wo
+print(s3)  # lo wo
+print(s4)  # olleh
+print(s5)  # hello'''
+
+
+#operations on string
+#concatenation and repetition
+'''s1 = "hello"
+s2 = "world"
+s3 = s1 + " " + s2
+s4 = s1 * 3
+print(s3)  # hello world
+print(s4)  # hellohellohello'''
+
+#Membership testing
+'''s1 = "hello world"
+print('h' in s1)  # True
+print('z' in s1)  # False
+print('lo' in s1)  # True
+print('lo' not in s1)  # False
+print('z' not in s1)  # True'''
+
+#relational operators // string comparison(Dictionary order)
+'''s1 = "hello"
+s2 = "world"
+s3 = "hello"
+print(s1 == s2)  # False
+print(s1 == s3)  # True
+print(s1 != s2)  # True'''
+
+
+
+
---
+++ string/string_projects.py
@@ -0,0 +1,42 @@
+# String Projects
+'''a = 'restaurant Menu'
+tot_len = 20
+m1 = 'Hot Dog'
+m2 = 'Donuts'
+m3 = 'Burger'
+m4 = 'Pizza'
+m1p = 30
+m2p = 40
+m3p = 50
+m4p = 60
+print(f'{a:^{tot_len}}')
+print(f'{m1}  {("-" * (tot_len - (len(m1) + len(str(m1p)))))}${m1p}')
+print(f'{m2}  {("-" * (tot_len - (len(m2) + len(str(m2p)))))}${m2p}')
+print(f'{m3}  {("-" * (tot_len - (len(m3) + len(str(m3p)))))}${m3p}')
+print(f'{m4}  {("-" * (tot_len - (len(m4) + len(str(m4p)))))}${m4p}')'''
+
+
+
+#card payment receipt
+
+'''card_no = input('enter your 16 digit card number with spaces after 4 digits: ')
+last_4 = card_no[15: ]
+a = 1000
+four = 'X'* 4 + ' '
+display = four *3 + last_4
+print(f'amount {a} paid with card number {display}')
+#print(f'{'X'* 4 + ' ' + 'X'* 4 + ' ' + 'X'* 4 + ' '} {last_4}')'''
+
+
+#card payment receipt for 5 customers
+'''no = 5
+for i in range(no):
+    card_no = input('enter your 16 digit card number with spaces after 4 digits: ')
+    print(i)
+    last_4 = card_no[15: ]
+    a = 1000
+    four = 'X'* 4 + ' '
+    display = four *3 + last_4
+    print(f'amount {a} paid with card number {display}')'''
+    
+    
